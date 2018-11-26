d9154352a81dda0a130c2e09b054bcd9b68faa6a
Paul Le Grand Des Cloizeaux <paul.legranddec@gmail.com>
2018-11-27 00:18:22 +0100

    run for tests

---
+++ run.py
@@ -0,0 +1,7 @@
+from RMSprop_perceptron import SGD_builder, RMS_builder, ADAM_builder
+from keras_cnn_mnist import CNN_factory
+from utils import multi_runs_mnist
+
+batches_size = [16, 32, 64, 128]
+names = [f'data/CNN-batchsize{s}.csv' for s in batches_size]
+multi_runs_mnist(1, 1, names, CNN_factory, batches_size=batches_size)
