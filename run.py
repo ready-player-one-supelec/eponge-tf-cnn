b2cdfa2968feb848c1d71446aa45c1bc4680603a
Paul Le Grand Des Cloizeaux <paul.legranddec@gmail.com>
2018-12-03 02:11:49 +0100

    run with time

--- run.py
+++ run.py
@@ -3,5 +3,6 @@ from keras_cnn_mnist import CNN_factory
 from utils import multi_runs_mnist
 
 batches_size = [16, 32, 64, 128]
-names = [f'data/CNN-batchsize{s}.csv' for s in batches_size]
-multi_runs_mnist(10, 10, names, CNN_factory, batches_size=batches_size)
+names = [f'data/CNN-batchsize{s}' for s in batches_size]
+multi_runs_mnist(1, 1, names, CNN_factory, batches_size=batches_size, metrics=[
+                 ('sample', 'error_rate'), ('sample', 'time')])
